a6c006b1ff3d980c0d90ef4a0f341a92b0720e0a
EnglishGhost <adamccastell@gmail.com>
2020-12-10 01:55:00 -0600

    Completed homepage
    video ended at 3:09:45. we have the entire homepage correct and now moving onto other parts of the site.

--- streamservice/src/app.js
+++ streamservice/src/app.js
@@ -1,21 +1,29 @@
 import React from 'react'
-import jumboData from './fixtures/jumbo';
-import Jumbotron from './components/jumbotron';
+import * as ROUTES from './constants/routes'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import {
+  Home,
+  Browse,
+  Signin,
+  Signup
+} from './pages/index'
+
 
 export default function App() {
   return (
-    <Jumbotron.Container>
-      {jumboData.map((item) => (
-        <Jumbotron key={item.id} direction={item.direction}>
-          <Jumbotron.Pane>
-            <Jumbotron.Title>{item.title}</Jumbotron.Title>
-            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-          </Jumbotron.Pane>
-          <Jumbotron.Pane>
-            <Jumbotron.Image src={item.image} alt={item.alt} />
-          </Jumbotron.Pane>
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
+    <Router>
+      <Route exact path="/browse">
+        <Browse />
+      </Route>
+      <Route exact path="/signin">
+        <Signin />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
   );
 }
---
+++ streamservice/src/pages/Signup.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { JumbotronContainer } from '../containers/jumbotron'
+import { FooterContainer } from '../containers/footer'
+import {FaqsContainer } from '../containers/faqs'
+
+export default function Signup() {
+  return (
+    <>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </>
+  );
+}
